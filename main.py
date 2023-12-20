1b9253800d2df9173114220fc25ecf0d33bf388c
achintya.chaudhary@github.com <pevkib-3zykqu-wuVmeq>
2023-12-20 15:41:47 +0530

    /

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+# if you fear out of index it can go, then you can start from first index if it is given in constraint
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
