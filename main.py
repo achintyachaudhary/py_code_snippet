c79c3f37ec067d60777ec66cd2869a97bcc3d57f
achintya.chaudhary@github.com <pevkib-3zykqu-wuVmeq>
2024-01-15 15:45:35 +0530

    d

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 # if you fear out of index it can go, then you can start from first index if it is given in constraint
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
@@ -9,3 +10,20 @@ class Solution:
                 nums[j] = nums[i]
                 j += 1
         return j
+
+
+# TODO:
+x = {1: 2}
+print(*x)
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        currentSum = nums[0]
+
+        for num in nums[1:]:
+            currentSum = max(num, currentSum + num)
+            maxSum = max(maxSum, currentSum)
+
+        return maxSum
